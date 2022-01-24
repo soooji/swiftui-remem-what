28824e62a2db443fba349c116d205a71aedad561
Sajad Beheshti <beheshtisajad@gmail.com>
2022-01-24 06:20:34 +0330

    Initial Commit

---
+++ Shared/SoojiUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoojiUIApp.swift
+//  Shared
+//
+//  Created by NiC on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SoojiUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
